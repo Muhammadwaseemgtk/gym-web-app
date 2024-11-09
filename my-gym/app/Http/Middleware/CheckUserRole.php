afc255e4c022aa2f6dbd7960276675703c31f2e7
Muhammad Waseem <Muhammadwaseemgtk@gmail.com>
2024-11-10 01:01:18 +0500

    created a custom middleware to handle which user allowed to view a specific dashboard according to its role

---
+++ my-gym/app/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  string  $role
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next, String $role): Response
+    {
+        if (auth()->user()->role !== $role) {
+            return redirect()->route('dashboard');
+        }
+        return $next($request);
+    }
+}
